fc48da34f31d711e30de7833c3bb5db14c57b424
Coder Club <hi@coderclub.com>
2023-04-09 23:11:44 +0300

    Recap

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear);
+            }
+
+            
+        }
+    }
+}
